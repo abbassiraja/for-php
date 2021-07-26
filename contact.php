e2a11d2e88fc0e33d3ee10cf06e9c1c5f457451a
abbassiraja <abbassirajap4@gmail.com>
2021-07-26 10:19:44 +0200

    projet3

---
+++ contact.php
@@ -0,0 +1,44 @@
+<?php
+
+echo '<table border 2>';
+
+echo '<tr>';
+echo '<td>'.'nom'.'</td>';
+echo '<td>'.'prénom'.'</td>';
+echo '<td>'.'cin'.'</td>';
+echo '<td>'.'adress'.'</td>';
+echo '<td>'.'code_postal'.'</td>';
+echo'</tr>';
+
+echo '<tr>';
+
+echo '<td>'.$_GET['name'].'</td>';
+echo '<td>'.$_GET['prénom'].'</td>';
+echo '<td>'.$_GET['cin'].'</td>'; 
+echo '<td>'.$_GET['adress'].'</td>';
+echo '<td>'.$_GET['codpostal'].'</td>';
+ echo'</tr>';
+echo '</table>';
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
